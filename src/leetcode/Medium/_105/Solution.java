7727f66ee4391578a946fe237e5ad6cf904c84b5
Myroslav Vivcharyk <vm.analytic@gmail.com>
2020-10-09 15:43:35 +0300

    Added solution for leetcode 133 problem.

--- src/leetcode/Medium/_105/Solution.java
+++ src/leetcode/Medium/_105/Solution.java
@@ -2,24 +2,28 @@ package leetcode.Medium._105;
 
 // https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
+import java.util.Objects;
+
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         return build(0, 0, inorder.length - 1, preorder, inorder);
     }
 
-    public TreeNode build(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-        if (preStart > preorder.length - 1 || inStart > inEnd) {
-            return null;
-        }
+    private TreeNode build(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart < 0 || preStart > preorder.length - 1 || inStart > inEnd) return null;
+
         TreeNode root = new TreeNode(preorder[preStart]);
-        int inIndex = 0; // Index of current root in inorder
-        for (int i = inStart; i <= inEnd; i++) {
-            if (inorder[i] == root.val) {
+
+        int inIndex = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (root.val == inorder[i]) {
                 inIndex = i;
             }
         }
+
         root.left = build(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-        root.right = build(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        root.right = build(preStart + inIndex - inStart + 1,inIndex + 1, inEnd, preorder, inorder);
+
         return root;
     }
 }
@@ -34,5 +38,20 @@ class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TreeNode)) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val &&
+                Objects.equals(left, treeNode.left) &&
+                Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
     }
 }
