04155ba26d2a72c9139393c796885a2d12d84541
Myroslav Vivcharyk <vm.analytic@gmail.com>
2020-10-14 18:08:39 +0300

    Added solution for leetcode problem 242.

---
+++ src/leetcode/Easy/_242/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.Easy._242;
+
+// https://leetcode.com/problems/valid-anagram/
+public class Solution {
+    //Time: O(n); Space: O(1)
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] counter = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+
+        for (int n: counter) {
+            if (n != 0) return false;
+        }
+
+        return true;
+    }
+}
