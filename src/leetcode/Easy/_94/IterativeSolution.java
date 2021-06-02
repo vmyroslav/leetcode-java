12e585b3bb981ab213ac712908da7dc358cc33a2
Myroslav Vivcharyk <mviv@ciklum.com>
2021-06-02 22:44:13 +0300

    Added solution for task 94.

---
+++ src/leetcode/Easy/_94/IterativeSolution.java
@@ -0,0 +1,27 @@
+package leetcode.Easy._94;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class IterativeSolution {
+    // Time: O(n); O(n)
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pointer = root;
+
+        while (pointer != null || !stack.isEmpty()) {
+            while (pointer != null) {
+                stack.push(pointer);
+                pointer = pointer.left;
+            }
+
+            pointer = stack.pop();
+            res.add(pointer.val);
+            pointer = pointer.right;
+        }
+
+        return res;
+    }
+}
